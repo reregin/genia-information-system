61b8e67d7c26b0d6ef17aa4b4bb31c09ba6ca5be
idoo0 <ridhoaditya925@gmail.com>
2025-06-04 18:25:36 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -90,12 +90,12 @@ Route::prefix('admin')->name('admin.')->group(function () {
             return view('modules.admin.competition.add');
         })->name('competition.add');
       
-        Route::get('/competitions', [App\Http\Controllers\AdminCompetitionController::class, 'index'])->name('admin.competition');
-        Route::get('/competitions/create', [App\Http\Controllers\AdminCompetitionController::class, 'create'])->name('admin.competition.create');
-        Route::post('/competitions', [App\Http\Controllers\AdminCompetitionController::class, 'store'])->name('admin.competition.store');
-        Route::get('/competitions/{id}/edit', [App\Http\Controllers\AdminCompetitionController::class, 'edit'])->name('admin.competition.edit');
-        Route::put('/competitions/{id}', [App\Http\Controllers\AdminCompetitionController::class, 'update'])->name('admin.competition.update');
-        Route::delete('/competitions/{id}', [App\Http\Controllers\AdminCompetitionController::class, 'destroy'])->name('admin.competition.destroy');
+        Route::get('/competitions', [App\Http\Controllers\AdminCompetitionController::class, 'index'])->name('competition');
+        Route::get('/competitions/create', [App\Http\Controllers\AdminCompetitionController::class, 'create'])->name('competition.create');
+        Route::post('/competitions', [App\Http\Controllers\AdminCompetitionController::class, 'store'])->name('competition.store');
+        Route::get('/competitions/{id}/edit', [App\Http\Controllers\AdminCompetitionController::class, 'edit'])->name('competition.edit');
+        Route::put('/competitions/{id}', [App\Http\Controllers\AdminCompetitionController::class, 'update'])->name('competition.update');
+        Route::delete('/competitions/{id}', [App\Http\Controllers\AdminCompetitionController::class, 'destroy'])->name('competition.destroy');
 
       
 
@@ -105,26 +105,26 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::post('/news', [App\Http\Controllers\Admin\NewsController::class, 'store'])->name('news.store');
         Route::get('/news/edit/{id}', [App\Http\Controllers\Admin\NewsController::class, 'edit'])->name('news.edit');
         Route::put('/news/{id}', [App\Http\Controllers\Admin\NewsController::class, 'update'])->name('news.update');
-        Route::delete('/news/{id}', [NewsController::class, 'destroy'])->name('admin.news.destroy');
+        Route::delete('/news/{id}', [App\Http\Controllers\Admin\NewsController::class, 'destroy'])->name('admin.news.destroy');
 
 //         Route::get('/blog', function () {
 //             return view('modules.admin.blog.manage');
 //         })->name('admin.blog');
         // Blog
-        Route::get('/blog', [AdminBlogController::class, 'index'])->name('admin.blog');
-        Route::get('/blog/add', [AdminBlogController::class, 'create'])->name('admin.blog.add');
-        Route::post('/blog/add', [AdminBlogController::class, 'store'])->name('admin.blog.store');
-        Route::get('/blog/edit/{blog?}', [AdminBlogController::class, 'edit'])->name('admin.blog.edit');
-        Route::put('/blog/edit/{blog}', [AdminBlogController::class, 'update'])->name('admin.blog.update');
-        Route::delete('/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blog.delete');
+        Route::get('/blog', [AdminBlogController::class, 'index'])->name('blog');
+        Route::get('/blog/add', [AdminBlogController::class, 'create'])->name('blog.add');
+        Route::post('/blog/add', [AdminBlogController::class, 'store'])->name('blog.store');
+        Route::get('/blog/edit/{blog?}', [AdminBlogController::class, 'edit'])->name('blog.edit');
+        Route::put('/blog/edit/{blog}', [AdminBlogController::class, 'update'])->name('blog.update');
+        Route::delete('/{blog}', [AdminBlogController::class, 'destroy'])->name('blog.delete');
 
         Route::get('/blog/add', function () {
             return view('modules.admin.blog.add');
-        })->name('admin.blog.add');
+        })->name('blog.add');
 
         Route::get('/blog/edit', function () {
             return view('modules.admin.blog.edit');
-        })->name('admin.blog.edit');
+        })->name('blog.edit');
 
         // News
         Route::get('/news', function () {
@@ -135,11 +135,11 @@ Route::prefix('admin')->name('admin.')->group(function () {
                 1   // Current page
             );
             return view('modules.admin.news.manage', compact('news'));
-        })->name('admin.news');
+        })->name('news');
 
         Route::get('/news/add', function () {
             return view('modules.admin.news.add');
-        })->name('admin.news.add');
+        })->name('news.add');
 
         Route::get('/news/edit/{id}', function ($id) {
             return view('modules.admin.news.edit', ['news' => (object)[
@@ -155,18 +155,18 @@ Route::prefix('admin')->name('admin.')->group(function () {
                 'publish_date' => '',
                 'slug' => ''
             ]]);
-        })->name('admin.news.edit');
+        })->name('news.edit');
 
         // API routes for news management
         Route::put('/news/{id}', function ($id) {
             // Update news logic here
             return redirect()->route('admin.news');
-        })->name('admin.news.update');
+        })->name('news.update');
 
         Route::delete('/news/{id}', function ($id) {
             // Delete news logic here
             return response()->json(['success' => true]);
-        })->name('admin.news.delete');
+        })->name('news.delete');
 
     });
 });
